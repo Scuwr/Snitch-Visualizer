eae787cebe37cbe3866da8b7b401222af41123f3
Scuwr <theScuwr@gmail.com>
2015-03-21 20:01:55 -0600

    commented SVConfig.java

--- src/main/java/com/github/scuwr/snitchvisualizer/classobjects/SVConfig.java
+++ src/main/java/com/github/scuwr/snitchvisualizer/classobjects/SVConfig.java
@@ -1,10 +1,22 @@
 package com.github.scuwr.snitchvisualizer.classobjects;
 
+/**
+ * Contains methods pertaining the config and config options
+ * 
+ * @author Scuwr
+ *
+ */
 public class SVConfig {
 	
 	public static boolean rendering;
 	public static boolean updateDetection;
 	
+	/**
+	 * Change plugin settings
+	 * 
+	 * @param rendering enable snitch rendering on-screen
+	 * @param updateDetection enable auto-update when a players moves through the snitch
+	 */
 	public SVConfig(boolean rendering, boolean updateDetection){
 		this.rendering = rendering;
 		this.updateDetection = updateDetection;
